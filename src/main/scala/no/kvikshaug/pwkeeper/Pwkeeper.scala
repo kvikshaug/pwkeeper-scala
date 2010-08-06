942127918a6b6c290e889a5be673e7a15d0d5e3b
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-08-06 17:13:34 +0200

    Merge keeper and getter into one file

--- src/main/scala/no/kvikshaug/pwkeeper/Pwkeeper.scala
+++ src/main/scala/no/kvikshaug/pwkeeper/Pwkeeper.scala
@@ -1,16 +1,20 @@
 package no.kvikshaug.pwkeeper
 
-import java.security.{KeyPairGenerator, NoSuchAlgorithmException, KeyPair}
-import javax.crypto.{Cipher}
-import javax.crypto.spec.SecretKeySpec
-import java.io.{BufferedOutputStream, FileOutputStream, File}
+import javax.crypto.Cipher
+import javax.crypto.spec.{SecretKeySpec, IvParameterSpec}
+import java.io.{BufferedOutputStream, FileOutputStream, BufferedInputStream, FileInputStream, File}
 
-object Pwkeeper {
+object Pwgetter {
+
+  val clearTextFile = new File("orig")
+  val encryptedFile = new File("data")
+
+  val cipher = "AES/CBC/PKCS5PADDING"
+  val algorithm = "AES"
 
   def main(args: Array[String]) = {
-    val c = Cipher.getInstance("AES/CBC/PKCS5PADDING")
     val key = "1234567890123456".getBytes
-    val data = scala.io.Source.fromFile("orig").mkString.getBytes
+    val data = scala.io.Source.fromFile(clearTextFile).mkString.getBytes
     val k = new SecretKeySpec(key, "AES");
     c.init(Cipher.ENCRYPT_MODE, k);
     val encData = c.doFinal(data);
@@ -22,9 +26,20 @@ object Pwkeeper {
     b.flush
   }
 
-  def makeKey(alg: String) = {
-    val keyPair = KeyPairGenerator.getInstance(alg).generateKeyPair
-    println(keyPair.getPublic)
-    println(keyPair.getPrivate)
-   }
+  def decrypt = {
+    val c = Cipher.getInstance(cipher)
+    val key = "1234567890123456".getBytes
+    val in = new BufferedInputStream(new FileInputStream(encryptedFile))
+    val iv = new Array[Byte](16)
+    val data = new Array[Byte]((file.length - 16).asInstanceOf[Int])
+    in.read(iv, 0, iv.size)
+    in.read(data, 0, data.length)
+    in.close
+    val k = new SecretKeySpec(key, "AES");
+    c.init(Cipher.DECRYPT_MODE, k, new IvParameterSpec(iv));
+    val encData = c.doFinal(data);
+
+    println(new String(encData))
+  }
+
 }
