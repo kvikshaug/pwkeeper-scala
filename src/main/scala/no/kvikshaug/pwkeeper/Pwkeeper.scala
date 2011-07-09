1d5e02148bcbff86c192aacfc55ee873389848c3
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-07-09 17:13:00 +0200

    Handle exceptions casually
    Instead of cluttering the code to provide user-friendly messages for
    exceptions, just print the exception message and fail.

--- src/main/scala/no/kvikshaug/pwkeeper/Pwkeeper.scala
+++ src/main/scala/no/kvikshaug/pwkeeper/Pwkeeper.scala
@@ -9,30 +9,29 @@ object Pwkeeper {
   val tmpFile = new File("tmp")
 
   def main(args: Array[String]): Unit = {
-    args.toList match {
-      case "generate" :: Nil        => println(Pwgen.generate())
-      case "generate" :: len :: Nil => println(Pwgen.generate(len.toInt))
-      case "read" :: Nil     => read
-      case "save" :: Nil     => save
-      case "search" :: Nil          => search()
-      case Nil                      => search()
-      case _ => println("Wrong usage.")
+    try {
+      args.toList match {
+        case "generate" :: Nil        => println(Pwgen.generate())
+        case "generate" :: len :: Nil => println(Pwgen.generate(len.toInt))
+        case "read" :: Nil            => read
+        case "save" :: Nil            => save
+        case "search" :: Nil          => search()
+        case Nil                      => search()
+        case _ => println("Wrong usage.")
+      }
+    } catch {
+      case e => println("Operation failed: " + e)
+      System.exit(-1)
     }
   }
 
   def search() {}
 
   def read {
-    try {
-      // decrypt the file and write it to a temporary file
-      val decryptedData = Crypt.decrypt(IO.read(encryptedFile))
-      IO.write(decryptedData, tmpFile)
-      println()
-    } catch {
-      case e: FileNotFoundException => println("The expected encrypted file doesn't exist at '" +
-        encryptedFile.getAbsolutePath + "'.")
-      case e => println("Decryption failed: " + e); System.exit(-1)
-    }
+    // decrypt the file and write it to a temporary file
+    val decryptedData = Crypt.decrypt(IO.read(encryptedFile))
+    IO.write(decryptedData, tmpFile)
+    println()
   }
 
   def save {
