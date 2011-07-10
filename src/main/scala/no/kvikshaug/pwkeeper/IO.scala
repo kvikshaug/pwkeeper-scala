e26266c342200be681f06d481e0035a561a3de04
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-07-10 17:40:41 +0200

    Convert tabs to spaces
    BAD SUBLIME! BAD!

--- src/main/scala/no/kvikshaug/pwkeeper/IO.scala
+++ src/main/scala/no/kvikshaug/pwkeeper/IO.scala
@@ -3,17 +3,17 @@ package no.kvikshaug.pwkeeper
 import java.io.{BufferedOutputStream, FileOutputStream, BufferedInputStream, FileInputStream, File}
 
 object IO {
-	def read(f: File): Array[Byte] = {
+  def read(f: File): Array[Byte] = {
     val in = new BufferedInputStream(new FileInputStream(f))
     val data = new Array[Byte](f.length.asInstanceOf[Int])
     in.read(data, 0, data.length)
     in.close
     data
-	}
+  }
 
-	def write(b: Array[Byte], f: File) = {
+  def write(b: Array[Byte], f: File) = {
     val out = new BufferedOutputStream(new FileOutputStream(f))
     out.write(b, 0, b.size)
     out.close
-	}
+  }
 }
